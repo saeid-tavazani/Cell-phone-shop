4b1c0ed15cc49611c1eb4c94a8f4e7c38611ea17
saeid tavazani <saeidtavazani@gmail.com>
2024-02-21 04:35:36 -0800

    fix slider

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -2,14 +2,10 @@ import Carousel from "react-bootstrap/Carousel";
 import { slider } from "../data";
 export default function Slider() {
   return (
-    <Carousel data-bs-theme="dark" className="rounded-3 overflow-hidden">
+    <Carousel className="rounded-3 overflow-hidden">
       {slider.map((item) => (
         <Carousel.Item key={item.id} className="">
           <img className="d-block w-100" src={item.img} alt={item.title} />
-          <Carousel.Caption>
-            <h5>{item.title}</h5>
-            <p>{item.description}</p>
-          </Carousel.Caption>
         </Carousel.Item>
       ))}
     </Carousel>
