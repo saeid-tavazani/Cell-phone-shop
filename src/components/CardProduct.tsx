06662a1e6ce29644da790ee162d1e9aa2d710f5f
saeid tavazani <saeidtavazani@gmail.com>
2024-02-21 08:09:26 -0800

    fix cart

--- src/components/CardProduct.tsx
+++ src/components/CardProduct.tsx
@@ -1,4 +1,4 @@
-import { Card, Button } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 import { useCartContext } from "../context/CartContext";
 import ShoppingCartManagement from "./ShoppingCartManagement";
 type PropType = {
@@ -18,19 +18,28 @@ export default function CardProduct({
   const { getItemQty } = useCartContext();
   const qty: number = getItemQty(id);
   return (
-    <Card className="py-3 px-1" style={{ height: "412px" }}>
+    <Card
+      className="py-3 px-1 d-flex flex-column justify-content-between"
+      style={{ height: "445px" }}
+    >
       <Card.Img
         style={{ height: "240px", maxWidth: "252px" }}
         variant="top"
         src={img}
       />
-      <Card.Body className="pb-0">
+      <Card.Body className="pb-0 d-flex flex-column justify-content-between">
         <Card.Title
-          className="h-5 "
+          className="h-5 m-0"
           style={{ fontSize: "1rem", lineHeight: 2, fontWeight: 600 }}
         >
           {title.length > 47 ? title.substring(0, 47).concat("...") : title}
         </Card.Title>
+        <div className="d-flex align-items-center w-100 justify-content-between">
+          <span className={quantity == 0 ? "text-danger" : "text-success"}>
+            {quantity === 0 ? "ناموجود" : quantity}
+          </span>
+          <span>{price.toString().concat(" تومان")}</span>
+        </div>
         <ShoppingCartManagement qty={qty} id={id} />
       </Card.Body>
     </Card>
